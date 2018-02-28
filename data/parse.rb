4e04700407ce890bf68916e4d92c7b91097f815f
Jack Stratton <jack@phroa.net>
2018-02-28 10:19:02 -0800

    Parse courses

--- data/parse.rb
+++ data/parse.rb
@@ -14,18 +14,31 @@ Term = Struct.new :code, :year, :quarter, :current
 Attribute = Struct.new :code, :desc, :gur
 Subject = Struct.new :code, :desc
 Instructor = Struct.new :code, :last, :first
+Course = Struct.new :code,
+                    :subject,
+                    :number,
+                    :crn,
+                    :title,
+                    :location,
+                    :times,
+                    :days,
+                    :capacity,
+                    :enrolled,
+                    :available,
+                    :instructor,
+                    :credits
 
 contents = File.read 'index.html'
 
 index = Nokogiri::HTML::Document.parse contents
 
-all_terms = (index .xpath("//select[@name='term']/option/@value")
+all_terms = (index.xpath("//select[@name='term']/option/@value")
                .map(&:value)
                .map do |term|
                Term.new(term, term[0..3].to_i, TERM_MAP[term[4..6]], false)
              end)
 
-current_term = (index .xpath("//select[@name='term']/option[@selected]/@value")
+current_term = (index.xpath("//select[@name='term']/option[@selected]/@value")
                   .map(&:value)
                   .map do |term|
                   Term.new(term, term[0..3].to_i, TERM_MAP[term[4..6]], true)
@@ -33,7 +46,7 @@ current_term = (index .xpath("//select[@name='term']/option[@selected]/@value")
 
 all_terms.find(current_term).first.current = true
 
-all_attributes = (index .xpath("//select[@name='sel_gur']/option/text()")
+all_attributes = (index.xpath("//select[@name='sel_gur']/option/text()")
                     .drop(1)
                     .map(&:text)
                     .map(&:strip)
@@ -41,7 +54,7 @@ all_attributes = (index .xpath("//select[@name='sel_gur']/option/text()")
                     Attribute.new(*x.split(" - "), x.include?('GUR'))
                   end)
 
-all_subjects = (index .xpath("//select[@name='sel_subj']/option/text()")
+all_subjects = (index.xpath("//select[@name='sel_subj']/option/text()")
                   .drop(1)
                   .map(&:text)
                   .map(&:strip)
@@ -49,14 +62,18 @@ all_subjects = (index .xpath("//select[@name='sel_subj']/option/text()")
                   Subject.new(*x.split(" - "))
                 end)
 
-all_instructors = (index .xpath("//select[@name='sel_inst']/option")
+all_instructors = (index.xpath("//select[@name='sel_inst']/option")
                      .drop(1)
                      .map do |x|
                      Instructor.new(x.attr(:value), *x.text.strip.split(', '))
                    end)
 
+all_courses = {}
+
 all_terms.each do |term|
   FileUtils.mkdir_p "/data/cache/#{term.code}"
+
+  all_courses[term] = {}
 
   Dir.chdir "/data/cache/#{term.code}" do
     all_subjects.each do |subject|
@@ -66,6 +83,11 @@ all_terms.each do |term|
           -H 'Content-Type: application/x-www-form-urlencoded'\
           --data 'sel_subj=dummy&sel_subj=dummy&sel_gur=dummy&sel_gur=dummy&sel_day=dummy&sel_open=dummy&sel_crn=&term=#{CGI.escape(term.code)}&sel_gur=All&sel_subj=#{CGI.escape(subject.code)}&sel_inst=ANY&sel_crse=&begin_hh=0&begin_mi=A&end_hh=0&end_mi=A&sel_cdts=%25'`
       end
+
+      all_courses[term][subject] = []
+
+      subject_html = Nokogiri::HTML::Document.parse(File.read("#{CGI.escape(subject.code)}.html"))
+
     end
   end
 end
