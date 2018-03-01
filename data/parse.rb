2e1ab7d64fe7d8c71044178cbff273fefe6863de
Jack Stratton <jack@phroa.net>
2018-02-28 18:16:43 -0800

    Parse the first course
    It's in a `while`, but only the first course is parsed.  Also, the while never
    breaks.  Don't run this.

--- data/parse.rb
+++ data/parse.rb
@@ -1,4 +1,5 @@
 require 'cgi'
+require 'date'
 require 'fileutils'
 require 'nokogiri'
 
@@ -19,6 +20,7 @@ Course = Struct.new :code,
                     :number,
                     :crn,
                     :title,
+                    :attributes,
                     :location,
                     :times,
                     :days,
@@ -50,22 +52,22 @@ all_attributes = (index.xpath("//select[@name='sel_gur']/option/text()")
                     .drop(1)
                     .map(&:text)
                     .map(&:strip)
-                    .map do |x|
-                    Attribute.new(*x.split(" - "), x.include?('GUR'))
+                    .map do |attribute|
+                    Attribute.new(*attribute.split(" - "), attribute.include?('GUR'))
                   end)
 
 all_subjects = (index.xpath("//select[@name='sel_subj']/option/text()")
                   .drop(1)
                   .map(&:text)
                   .map(&:strip)
-                  .map do |x|
-                  Subject.new(*x.split(" - "))
+                  .map do |attribute|
+                  Subject.new(*attribute.split(" - "))
                 end)
 
 all_instructors = (index.xpath("//select[@name='sel_inst']/option")
                      .drop(1)
-                     .map do |x|
-                     Instructor.new(x.attr(:value), *x.text.strip.split(', '))
+                     .map do |attribute|
+                     Instructor.new(attribute.attr(:value), *attribute.text.strip.split(', '))
                    end)
 
 all_courses = {}
@@ -80,7 +82,7 @@ all_terms.each do |term|
       unless File.exists? "#{CGI.escape(subject.code)}.html"
         `curl -o "#{CGI.escape(subject.code)}.html" 'https://admin.wwu.edu/pls/wwis/wwsktime.ListClass'\
           -H 'Host: admin.wwu.edu' --compressed\
-          -H 'Content-Type: application/x-www-form-urlencoded'\
+          -H 'Content-Type: application/attribute-www-form-urlencoded'\
           --data 'sel_subj=dummy&sel_subj=dummy&sel_gur=dummy&sel_gur=dummy&sel_day=dummy&sel_open=dummy&sel_crn=&term=#{CGI.escape(term.code)}&sel_gur=All&sel_subj=#{CGI.escape(subject.code)}&sel_inst=ANY&sel_crse=&begin_hh=0&begin_mi=A&end_hh=0&end_mi=A&sel_cdts=%25'`
       end
 
@@ -88,6 +90,42 @@ all_terms.each do |term|
 
       subject_html = Nokogiri::HTML::Document.parse(File.read("#{CGI.escape(subject.code)}.html"))
 
+      cells = subject_html
+                .css('table')[1]
+                .css('td')
+                .drop(11)
+
+      while cells.any?
+        course = Course.new
+
+        course.subject = subject
+        course.code = cells.shift.text
+        course.number = course.code.split[1].to_i
+        course.title = cells.shift.text
+        course.crn = cells.shift.xpath('input').attr('value').value.to_i
+        course.capacity = cells.shift.text.to_i
+        course.enrolled = cells.shift.text.to_i
+        course.available = cells.shift.text.to_i
+
+        ilast, ifirst = cells.shift.text.split(',').map(&:strip)
+        course.instructor = (all_instructors.find do |instructor|
+                               instructor.first == ifirst && instructor.last == ilast
+                             end)
+
+        sday, eday = (cells.shift.text.split('-').map do |range|
+                        Date.new(term.year, *range.split('/').map(&:to_i))
+                      end)
+        course.days = sday..eday
+
+        cells.shift
+
+        course.attributes = (cells.shift.text.split.map do |code|
+                               all_attributes.find do |attribute|
+                                 attribute.code == code
+                               end
+                             end)
+        all_courses[term][subject] << course
+      end
     end
   end
 end
